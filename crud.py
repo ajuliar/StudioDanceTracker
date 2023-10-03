dfd822a1b67a62656f17a72a4e47c8575349655d
ajuliar <ana.julia.mrr@gmail.com>
2023-10-03 18:18:33 -0500

    seed.py and crud.py done

--- crud.py
+++ crud.py
@@ -2,10 +2,10 @@
 
 from model import db, Admin, Student, Instructor, Class, StudentClass, connect_to_db
 
-def create_user(email,password):
+def create_admin(fname, lname, email,password):
     """Create and return a new admin."""
 
-    admin = Admin(email=email, password=password)
+    admin = Admin(fname=fname, lname=lname, email=email, password=password)
 
     return admin
 
@@ -21,3 +21,62 @@ def create_student(f_name, l_name, email, phone, address):
     )
 
     return student
+
+
+def create_instructor(first_name, last_name, email, phone):
+    """Create and return a new instructor"""
+
+    instructor = Instructor(
+        first_name=first_name,
+        last_name=last_name,
+        email=email,
+        phone=phone,
+    )
+
+    return instructor
+
+
+def create_class(class_name, schedule, start_date, end_date):
+    """Create a new class"""
+
+    a_class = Class(
+        class_name=class_name,
+        schedule=schedule,
+        start_date=start_date,
+        end_date=end_date,
+    )
+
+    return a_class
+
+def enroll_student_class(student_id, class_id):
+
+    enrollment = StudentClass(
+        student_id=student_id,
+        class_id=class_id,
+    )
+
+    return enrollment
+
+
+def get_admin_by_id(admin_id):
+    
+    return Admin.query.get(admin_id)
+
+
+def get_student_by_id(student_id):
+
+    return Student.query.get(student_id)
+
+def get_instructor_by_id(instructor_id):
+
+    return Instructor.query.get(instructor_id)
+
+def get_class_by_id(class_id):
+
+    return Class.query.get(class_id)
+    
+
+
+if __name__ == '__main__':
+    from server import app
+    connect_to_db(app)
--- seed.py
+++ seed.py
@@ -13,13 +13,38 @@ os.system('createdb students')
 model.connect_to_db(server.app)
 model.db.create_all()
 
-
-
-admin = crud.create_admin(email, password)
+fname = "Anna"
+lname = "Test"
+email = "admin@test.com"
+password = "test123"
+admin = crud.create_admin(fname,lname,email, password)
 model.db.session.add(admin)
 model.db.session.commit()
 
 
+students = [
+    {
+        "f_name": "Tara",
+        "l_name": "Davis",
+        "email": "tara@student.com",
+        "phone": "123-345-6789",
+        "address": "123 Oak St",
+    },
+    {
+        "f_name": "Alice",
+        "l_name": "Smith",
+        "email": "alice@student.com",
+        "phone": "123-345-9876",
+        "address": "123 Myrtle St",
+    },
+    {
+        "f_name": "John",
+        "l_name": "Williams",
+        "email": "john@student.com",
+        "phone": "123-345-3344",
+        "address": "123 Main St",
+    }
+]
 
 students_in_db = []
 for student in students:
@@ -35,4 +60,76 @@ for student in students:
     students_in_db.append(db_student)
 
 model.db.session.add_all(students_in_db)
+model.db.session.commit()
+
+
+instructors = [
+    {   
+        "first_name": "Jane",
+        "last_name": "Johnson",
+        "email": "jane@instructor.com",
+        "phone": "123-543-6789",  
+
+        },
+    {   
+        "first_name": "Sarah",
+        "last_name": "Smith",
+        "email": "sarah@instructor.com",
+        "phone": "321-345-6789",  
+
+    }
+        
+        
+]
+
+instructors_in_db = []
+for instructor in instructors:
+    first_name, last_name, email, phone = (
+        instructor["first_name"],
+        instructor["last_name"],
+        instructor["email"],
+        instructor["phone"],
+    )
+
+    db_instructor = crud.create_instructor(first_name, last_name, email, phone)
+    instructors_in_db.append(db_instructor)
+
+model.db.session.add_all(instructors_in_db)
+model.db.session.commit()
+
+db_classes = [
+    {   
+        "class_name": "Ballet Beginner",
+        "schedule": "Mon, Wed, Fri - 2pm to 3pm",
+        "start_date": "2023,01,25",
+        "end_date": "2023,07,05",
+
+    },
+    {
+        "class_name": "Jazz",
+        "schedule": "Wed, Fri - 4pm to 5pm",
+        "start_date": "2023,01,25",
+        "end_date": "2023,07,05",
+    },
+    {
+        "class_name": "Zumba",
+        "schedule": "Tue, Thu - 4pm to 5pm",
+        "start_date": "2023,01,25",
+        "end_date": "2023,07,05",
+    }
+]
+
+classes_in_db = []
+for db_class in db_classes:
+    class_name, schedule, start_date, end_date = (
+        db_class["class_name"],
+        db_class["schedule"],
+        db_class["start_date"],
+        db_class["end_date"],
+    )
+
+    class_database = crud.create_class(class_name, schedule, start_date, end_date)
+    classes_in_db.append(class_database)
+
+model.db.session.add_all(classes_in_db)
 model.db.session.commit()
